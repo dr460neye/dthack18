abe099b11d65d744c3cf460b5c511a803c2fcd49
Kina <kristinas_mail@gmx.de>
2018-09-05 19:07:54 +0200

    Update completeScan_MSuarez.ino

--- ExampleCodes/completeScan_MSuarez/completeScan_MSuarez.ino
+++ ExampleCodes/completeScan_MSuarez/completeScan_MSuarez.ino
@@ -5,6 +5,8 @@ ESP8266 Simple sniffer from  2018 Carve Systems LLC and  Angel Suarez-B Martin
 The adaptin we made is concerning the SSID from the Devices:
 Christin Koss: Adapted for the SSID fronm the sender DEVICE! ( ProbeRequest )
 
-in Build
+check out the original code:
+https://www.carvesystems.com/blog/advanced-esp-part-one-simple-sniffer/
+
 */
 
